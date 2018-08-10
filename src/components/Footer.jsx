6a2baaeae1c57a9e1e9aa53e2ff6c6bb6906d0b5
Lincoln Wisely <lincoln.wisely@manifest.com>
2018-08-10 09:28:34 -0500

    Create footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+// Practice with a stateless component
+// Const vs. class
+
+const Footer = props => {
+  return (
+    <header>
+      <h4>
+        Created by&nbsp;
+        <a href="https://github.com/lincolnwisely/">Lincoln Webber-Wisely</a>
+      </h4>
+    </header>
+  );
+};
+
+export default Footer;
